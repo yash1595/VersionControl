0d2ef1e1fd071a3d7abc16fbbee4f5871011f121
yash1595 <yagu9404@colorado.edu>
2019-08-27 23:41:18 -0600

    Periodic commit Update @ Tue 08/27/2019 23:41:18.35

--- git_batch.bat
+++ git_batch.bat
@@ -2,7 +2,7 @@ ECHO OFF
 ECHO Git Bash
 :loop
 git add -A
-git commit -m "Periodic commit Update @ %date%time"
+git commit -m "Periodic commit Update @ %date% %time%"
 git push https://github.com/yash1595/VersionControl master
 timeout 10
 ECHO Done Uploading
